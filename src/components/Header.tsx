ed89eecc9b26d43b770018c98cdd7d3722dd1df6
Ayechanpaing31 <paingaye20@gmail.com>
2023-11-19 17:41:06 +0800

    fixed ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import { signOut, useSession } from 'next-auth/react';
 import { useState } from 'react';
 
+
 const Header = () => {
   const { data: session } = useSession(); // Ensure you are using useSession hook correctly
   const [isMenuOpen, setMenuOpen] = useState(false);
-
+  
   const handleToggleMenu = () => {
     setMenuOpen(!isMenuOpen);
   };
@@ -22,7 +23,8 @@ const Header = () => {
       </button>
       <p>Dashboard</p>
       <div className="flex flex-col items-center justify-end gap-4">
-        <div className="text-center text-2xl text-black">
+        <div className="text-center text-s text-black font-bold" >
+          
           <div>
             <button
               type="button"
@@ -40,13 +42,16 @@ const Header = () => {
               </span>
             </button>
             {isMenuOpen && session && session.user && (
-              <div className="absolute top-14 right-0 bg-white p-4 rounded shadow-md">
+              <div className="absolute top-15 right-0 bg-white p-2 rounded shadow-md">
                 {/* Display user information */}
-                <p>Name: {session.user.name ?? 'Unknown'}</p>
-                <p>Email: {session.user.email ?? 'Unknown'}</p>
+                <p>{session.user.name ?? 'Unknown'}</p>
+                <p>{session.user.email ?? 'Unknown'}</p>
 
                 {/* Sign-out button */}
-                <button onClick={handleSignOut}>Sign Out</button>
+                <button 
+                onClick={handleSignOut}
+                className="p-2 mb-2 rounded-full bg-gray-800 text-white border-2 border-black "
+                >Sign Out</button>
               </div>
             )}
           </div>
