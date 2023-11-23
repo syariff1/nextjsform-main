f828faaa85c3cd03a5e31e8350d99939f8a60896
syariff1 <52456997+syariff1@users.noreply.github.com>
2023-11-23 20:38:27 +0800

    authshow part 5

--- src/components/AuthShowcase.tsx
+++ src/components/AuthShowcase.tsx
@@ -1,30 +1,14 @@
 import { signIn, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { api } from '~/utils/api';
-import { useEffect, useState } from 'react';
 
 export function AuthShowcase() {
   const router = useRouter();
   const { data: sessionData } = useSession();
-  const [secretMessage, setSecretMessage] = useState<string | null>(null);
-  const [error, setError] = useState<Error | null>(null);
-
-  useEffect(() => {
-    const fetchSecretMessage = async () => {
-      try {
-        const { data } = await api.post.getSecretMessage.useQuery(undefined, {
-          enabled: sessionData?.user !== undefined,
-        });
-        setSecretMessage(data || null); // Ensure `data` is not undefined
-      } catch (err: any) {
-        setError(err);
-      }
-    };
-
-    if (sessionData?.user !== undefined) {
-      fetchSecretMessage();
-    }
-  }, [sessionData]);
+  const { data: secretMessage, error } = api.post.getSecretMessage.useQuery(
+    undefined,
+    { enabled: sessionData?.user !== undefined }
+  );
 
   const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -38,15 +22,13 @@ export function AuthShowcase() {
     }
   };
 
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name || 'Unknown'}</span>}
-        {secretMessage !== null && <span> - {secretMessage}</span>}
-      </p>
-      {error ? (
-        <p>Error loading secret message</p>
-      ) : (
+  // Check for errors
+  if (error) {
+    console.error('Error loading secret message:', error);
+    // Handle the error, e.g., display an error message to the user
+    return (
+      <div className="flex flex-col items-center justify-center gap-4">
+        <p className="text-center text-2xl text-red-500">Error loading secret message</p>
         <form onSubmit={handleFormSubmit}>
           <button
             type="submit"
@@ -55,7 +37,25 @@ export function AuthShowcase() {
             {sessionData ? 'Go to Home' : 'Sign in'}
           </button>
         </form>
-      )}
+      </div>
+    );
+  }
+
+  // Render the component with the loaded data
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <p className="text-center text-2xl text-white">
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+        {secretMessage && <span> - {secretMessage}</span>}
+      </p>
+      <form onSubmit={handleFormSubmit}>
+        <button
+          type="submit"
+          className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        >
+          {sessionData ? 'Go to Home' : 'Sign in'}
+        </button>
+      </form>
     </div>
   );
 }
